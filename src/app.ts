f24ec9e20b9c5e6109b44be45d9e5eaa9e60d8e3
Matheus <matheusdeamorim20@gmail.com>
2023-09-20 15:30:57 -0300

    config: add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 export const app = fastify();
+
+const prisma = new PrismaClient();
+
+// prisma.user.create({
+//     data: {
+//         name: 'Matheus',
+//         email: 'matheusdeamorim20@gmail.com'
+//     }
+// })
+
+// prisma.user.findMany().then((users) => {
+//     console.log(users);
+// });
